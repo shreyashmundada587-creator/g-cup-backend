17f909fa089e54eb229fca75943f3c719faaf658
Shrey <shreyashmundada587@gmail.com>
2026-02-21 21:30:21 +0530

    Fixed supabaseClient exports

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,9 +1,11 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+const anonKey = process.env.SUPABASE_ANON_KEY
+const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseServiceKey
-)
+export const supabase = createClient(supabaseUrl, anonKey)
+
+export const createAdminClient = () => {
+  return createClient(supabaseUrl, serviceKey)
+}
